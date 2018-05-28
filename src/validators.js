c6fa9453417b8e42788215f02111b26a3311f20a
leonardom249 <leonardo.megan@gmail.com>
2018-05-28 14:52:04 -0500

    made minor updates to work with server

--- src/validators.js
+++ src/validators.js
@@ -1,8 +1,11 @@
 export const required = value => (value ? undefined : 'Required');
+
 export const nonEmpty = value =>
     value.trim() !== '' ? undefined : 'Cannot be empty';
+
 export const isTrimmed = value =>
     value.trim() === value ? undefined : 'Cannot start or end with whitespace';
+    
 export const length = length => value => {
     if (length.min && value.length < length.min) {
         return `Must be at least ${length.min} characters long`;
